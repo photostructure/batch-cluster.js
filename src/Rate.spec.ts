29dc1fafdabea730545e2afd6e21219c16ce8329
Matthew McEachen <matthew+github@mceachen.org>
2018-04-11 19:10:26 -0700

    rewrite to be simpler (no windows)

--- src/Rate.spec.ts
+++ src/Rate.spec.ts
@@ -1,5 +1,5 @@
+import { expect, times } from "./chai.spec"
 import { Rate } from "./Rate"
-import { expect, times } from "./spec"
 
 const tk = require("timekeeper")
 
@@ -14,8 +14,8 @@ describe("Rate", () => {
 
   after(() => tk.reset())
 
-  function expectRate(rate: Rate, epm: number, tol: number = 0.25) {
-    expect(rate.eventsPerMillisecond).to.be.withinToleranceOf(epm, tol)
+  function expectRate(rate: Rate, epm: number, tol: number = 0.5) {
+    expect(rate.eventsPerMs).to.be.withinToleranceOf(epm, tol)
     expect(rate.eventsPerSecond).to.be.withinToleranceOf(epm * 1000, tol)
     expect(rate.eventsPerMinute).to.be.withinToleranceOf(epm * 60 * 1000, tol)
   }
@@ -25,33 +25,38 @@ describe("Rate", () => {
   })
 
   it("maintains a rate of 0 after time", () => {
-    tk.freeze(now + r.windowMillis)
+    tk.freeze(now + r.ttlMs)
     expectRate(r, 0)
   })
 
   it("decays the rate as time elapses", () => {
     r.onEvent()
-    expectRate(r, 0)
-    tk.freeze(now + r.windowMillis)
-    expectRate(r, 0.5 / r.windowMillis) // .5, not 1, because it will be averaged with 0s
-    tk.freeze(now + 1.25 * r.windowMillis * r.windows)
+    expectRate(r, 1)
+    tk.freeze(now + 10)
+    expectRate(r, 0.1)
+    tk.freeze(now + r.ttlMs)
     expectRate(r, 0)
   })
 
   it("counts events from the same millisecond", () => {
     r.onEvent()
+    expectRate(r, 1)
     r.onEvent()
-    tk.freeze(now + r.windowMillis)
-    expectRate(r, 1 / r.windowMillis) // 1, not 2, because it will be averaged with 0s
+    expectRate(r, 2)
+    tk.freeze(now + 10)
+    expectRate(r, 2 / 10) // 1, not 2, because it will be averaged with 0s
+    tk.freeze(now + r.ttlMs)
+    expectRate(r, 0)
   })
-  ;[10, 100, 1000].forEach(events => {
+
+  for (const events of [10, 100, 1000]) {
     it("calculates average rate for " + events + " events", () => {
-      const period = r.windowMillis * r.windows
+      const period = r.ttlMs
       times(events, i => {
         tk.freeze(now + period * i / events)
         r.onEvent()
       })
-      expectRate(r, events / period, 0.1)
+      expectRate(r, events / period, 0.25)
     })
-  })
+  }
 })
--- src/Rate.ts
+++ src/Rate.ts
@@ -1,59 +1,54 @@
 export class Rate {
-  private _events: number = 0
+  private _eventCount: number = 0
   private start = Date.now()
-  private e = new Map<number, number>()
+  private store: number[] = []
 
-  constructor(
-    readonly windowMillis: number = 250,
-    readonly windows: number = 5
-  ) {}
+  constructor(readonly ttlMs: number = 60000) {}
 
   onEvent() {
-    this._events++
-    const t = this.asKey(Date.now())
-    this.e.set(t, (this.e.get(t) || 0) + 1)
+    this._eventCount++
+    this.store.push(Date.now())
     this.vacuum()
   }
 
-  get events(): number {
-    return this._events
+  get eventCount(): number {
+    return this._eventCount
   }
 
   get period(): number {
     return Date.now() - this.start
   }
 
-  get eventsPerMillisecond(): number {
-    let mean: number | undefined
-    const now = Date.now()
-    const key = this.asKey(now - this.windows * this.windowMillis)
-    // ignore the most recent window, which represents less than windowMillis
-    for (let window = 0; window < this.windows; window++) {
-      const events = this.e.get(key + window) || 0
-      const epms = events / this.windowMillis
-      mean = mean == null ? epms : (mean + epms) / 2
+  get eventsPerMs(): number {
+    this.vacuum()
+    const elapsed = Math.max(1, Date.now() - this.start)
+    if (elapsed > this.ttlMs) {
+      return this.store.length / this.ttlMs
+    } else {
+      return this.store.length / elapsed
     }
-    return mean || 0
   }
 
   get eventsPerSecond(): number {
-    return this.eventsPerMillisecond * 1000
+    return this.eventsPerMs * 1000
   }
 
   get eventsPerMinute(): number {
     return this.eventsPerSecond * 60
   }
 
-  private asKey(time: number): number {
-    return Math.floor(time / this.windowMillis)
+  clear(): this {
+    this.store.length = 0
+    return this
   }
 
   private vacuum() {
-    const least = this.asKey(Date.now() - this.windowMillis * this.windows)
-    this.e.forEach((_, k) => {
-      if (k < least) {
-        this.e.delete(k)
-      }
-    })
+    const minTime = Date.now() - this.ttlMs
+    const firstGoodIndex = this.store.findIndex(ea => ea > minTime)
+    if (firstGoodIndex == -1) {
+      this.clear()
+    } else if (firstGoodIndex > 0) {
+      this.store.splice(0, firstGoodIndex)
+    }
   }
 }
