199454d724b1ca674df4b8b02879f749eff924ab
Matthew McEachen <matthew@photostructure.com>
2019-03-11 09:54:29 -0700

    more rigorous test serial tests

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -44,14 +44,14 @@ describe("test.js", () => {
     async notRunning(): Promise<boolean> {
       return this.running().then(ea => !ea)
     }
-    async assertStdout(expectedOutput: string) {
+    async assertStdout(f: (output: string) => void) {
       // The OS may take a bit before the PID shows up in the process table:
       const alive = await until(() => pidExists(this.child.pid), 2000)
       expect(alive).to.eql(true)
       const d = new Deferred()
       this.child.on("exit", async () => {
         try {
-          expect(this.output.trim()).to.eql(expectedOutput)
+          f(this.output.trim())
           expect(await this.running()).to.eql(false)
           d.resolve()
         } catch (err) {
@@ -64,7 +64,9 @@ describe("test.js", () => {
 
   it("results in expected output", async () => {
     const h = new Harness()
-    const a = h.assertStdout("HELLO\nPASS\nworld\nPASS\nFAIL\nv1.2.3\nPASS")
+    const a = h.assertStdout(ea =>
+      expect(ea).to.eql("HELLO\nPASS\nworld\nPASS\nFAIL\nv1.2.3\nPASS")
+    )
     h.child.stdin!.end("upcase Hello\ndowncase World\ninvalid input\nversion\n")
     return a
   })
@@ -134,10 +136,25 @@ describe("test.js", () => {
   it("sleeps serially", () => {
     const h = new Harness()
     const start = Date.now()
+    const times = [200, 201, 202]
     const a = h
-      .assertStdout("slept 200\nPASS\nslept 201\nPASS\nslept 202\nPASS")
+      .assertStdout(output => {
+        const actualTimes: number[] = []
+        const pids = new Set()
+        output.split(/[\r\n]/).forEach(line => {
+          if (line.startsWith("{") && line.endsWith("}")) {
+            const json = JSON.parse(line)
+            actualTimes.push(json.slept)
+            pids.add(json.pid)
+          } else {
+            expect(line).to.eql("PASS")
+          }
+        })
+        expect(pids.size).to.eql(1, "only one pid should have been used")
+        expect(actualTimes).to.eql(times)
+      })
       .then(() => expect(Date.now() - start).to.be.gte(603))
-    h.child.stdin!.end("sleep 200\nsleep 201\nsleep 202\nexit\n")
+    h.child.stdin!.end(times.map(ea => "sleep " + ea).join("\n") + "\nexit\n")
     return a
   })
 
@@ -146,15 +163,17 @@ describe("test.js", () => {
     setRngseed("hello")
     const h = new Harness()
     // These random numbers are consistent because we have a consistent rngseed:
-    const a = h.assertStdout(
-      [
-        "flaky response (PASS, r: 0.55, flakeRate: 0.50)",
-        "PASS",
-        "flaky response (PASS, r: 0.44, flakeRate: 0.00)",
-        "PASS",
-        "flaky response (FAIL, r: 0.55, flakeRate: 1.00)",
-        "FAIL"
-      ].join("\n")
+    const a = h.assertStdout(ea =>
+      expect(ea).to.eql(
+        [
+          "flaky response (PASS, r: 0.55, flakeRate: 0.50)",
+          "PASS",
+          "flaky response (PASS, r: 0.44, flakeRate: 0.00)",
+          "PASS",
+          "flaky response (FAIL, r: 0.55, flakeRate: 1.00)",
+          "FAIL"
+        ].join("\n")
+      )
     )
     h.child.stdin!.end("flaky .5\nflaky 0\nflaky 1\nexit\n")
     return a
