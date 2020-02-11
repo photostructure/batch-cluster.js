cdddcc02037d5233efc9e31d8c3da066faecaa88
Matthew McEachen <matthew@photostructure.com>
2020-02-11 13:53:59 -0800

    add eslint-plugin-import

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,15 +7,20 @@ module.exports = {
     project: "tsconfig.json",
     sourceType: "module"
   },
-  plugins: ["@typescript-eslint"],
+  plugins: ["@typescript-eslint", "eslint-plugin-import"],
   rules: {
     "@typescript-eslint/no-extraneous-class": "warn",
     "@typescript-eslint/no-floating-promises": "warn",
     "@typescript-eslint/no-for-in-array": "warn",
     "@typescript-eslint/no-misused-new": "warn",
+    "@typescript-eslint/prefer-readonly": "warn",
     "@typescript-eslint/strict-boolean-expressions": "warn",
     "constructor-super": "warn",
     eqeqeq: ["warn", "smart"],
+    "import/first": "warn",
+    "import/no-cycle": "warn",
+    "import/no-duplicates": "warn",
+    "import/no-unassigned-import": "warn",
     "no-bitwise": "warn",
     "no-cond-assign": "warn",
     "no-debugger": "warn",
@@ -36,5 +41,7 @@ module.exports = {
     "no-unused-expressions": "warn",
     "no-var": "warn",
     "prefer-const": "warn"
+    // TODO re-enable when implemented?: https://github.com/typescript-eslint/typescript-eslint/blob/master/packages/eslint-plugin/ROADMAP.md
+    // "prefer-conditional-expression": true
   }
 }
