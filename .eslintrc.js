f0283e7ba7fa47a574f6a5b540480512bbf04fd2
Matthew McEachen <matthew@photostructure.com>
2021-07-05 16:40:06 -0700

    cleanup and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    node: true
+    node: true,
   },
   extends: [
     "plugin:@typescript-eslint/eslint-recommended",
@@ -10,23 +10,23 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "tsconfig.json",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["@typescript-eslint", "eslint-plugin-import"],
   rules: {
     "@typescript-eslint/explicit-function-return-type": "off",
     "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/member-delimiter-style": [ "warn", { multiline: { delimiter: "none" } } ],
+    "@typescript-eslint/member-delimiter-style": [
+      "warn",
+      { multiline: { delimiter: "none" } },
+    ],
     "@typescript-eslint/no-explicit-any": "off",
     "@typescript-eslint/no-var-requires": "off",
-    "eqeqeq": ["warn", "always", {"null": "ignore"}],
+    eqeqeq: ["warn", "always", { null: "ignore" }],
     "import/no-cycle": "warn",
     "import/no-unresolved": "off",
     "no-redeclare": "warn",
     "no-undef-init": "warn",
     "no-unused-expressions": "warn",
-    "node/no-missing-import": "off",
-    "node/no-unsupported-features/es-syntax": "off",
-    "node/shebang": "off",
-  }
+  },
 }
