61c7d31f32b1c684377cbb49e9a0850cc8a641ad
Matthew McEachen <matthew@photostructure.com>
2023-02-02 14:27:53 -0800

    Simplify pidExists() and kill() to rely on node:process

--- src/Pids.ts
+++ src/Pids.ts
@@ -3,81 +3,48 @@ import process from "process"
 import { map } from "./Object"
 import { isWin } from "./Platform"
 
-function safePid(pid: number) {
-  if (typeof pid !== "number" || pid < 0) {
-    throw new Error("invalid pid: " + JSON.stringify(pid))
-  } else {
-    return Math.floor(pid).toString()
-  }
-}
-
-/*
-
-Windows 10:
-
->tasklist /NH /FO "CSV" /FI "PID eq 15524"
-INFO: No tasks are running which match the specified criteria.
-
->tasklist /NH /FO "CSV" /FI "PID eq 11968" 
-"bash.exe","11968","Console","1","5,340 K"
-
-Linux:
-
-$ ps -p 20242 
-  PID TTY          TIME CMD
-20242 pts/3    00:00:00 bash
-
-Mac: 
-
-$ ps -p 32183
-  PID TTY           TIME CMD
-32183 ttys001    0:00.10 /bin/bash -l
-
-*/
-
 /**
  * @param {number} pid process id. Required.
- * @returns {Promise<boolean>} true if the given process id is in the local
- * process table. The PID may be paused or a zombie, though.
+ * @returns boolean true if the given process id is in the local process
+ * table. The PID may be paused or a zombie, though.
  */
-export function pidExists(pid: number | null | undefined): Promise<boolean> {
-  if (pid == null) return Promise.resolve(false)
-  const needle = safePid(pid)
-  const cmd = isWin ? "tasklist" : "ps"
-  const args = isWin
-    ? // NoHeader, FOrmat CSV, FIlter on pid:
-      [
-        ["/NH", "/FO", "CSV", "/FI", "PID eq " + needle],
-        {
-          windowsHide: true,
-        },
-      ]
-    : // linux has "quick" mode (-q) but mac doesn't. We add the ",1" to avoid ps
-      // returning exit code 1, which generates an extraneous Error.
-      [["-p", needle + ",1"]]
-  return new Promise((resolve) => {
-    child_process.execFile(
-      cmd,
-      ...(args as any),
-      (error: Error | null, stdout: string) => {
-        const result =
-          error == null &&
-          new RegExp(
-            isWin ? '"' + needle + '"' : "^\\s*" + needle + "\\b",
-            // The posix regex pattern needs multiline support:
-            "m"
-          ).exec(String(stdout).trim()) != null
-        resolve(result)
-      }
-    )
-  })
+export function pidExists(pid: number | undefined): boolean {
+  if (pid == null || !isFinite(pid) || pid <= 0) return false
+  try {
+    // signal 0 can be used to test for the existence of a process
+    // see https://nodejs.org/dist/latest-v18.x/docs/api/process.html#processkillpid-signal
+    return process.kill(pid, 0)
+  } catch (err) {
+    // failed to get priority--assume the pid is gone.
+  }
+  return false
+}
+
+/**
+ * Send a signal to the given process id.
+ *
+ * @export
+ * @param pid the process id. Required.
+ * @param force if true, and the current user has
+ * permissions to send the signal, the pid will be forced to shut down. Defaults to false.
+ */
+export function kill(pid: number | undefined, force = false): boolean {
+  if (pid == null || !isFinite(pid) || pid <= 0) return false
+  try {
+    return process.kill(pid, force ? "SIGKILL" : undefined)
+  } catch (err) {
+    if (!String(err).includes("ESRCH")) throw err
+    return false
+    // failed to get priority--assume the pid is gone.
+  }
 }
 
 const winRe = /^".+?","(\d+)"/
 const posixRe = /^\s*(\d+)/
 
 /**
- * @export
+ * Only used by tests
+ *
  * @returns {Promise<number[]>} all the Process IDs in the process table.
  */
 export function pids(): Promise<number[]> {
@@ -103,34 +70,4 @@ export function pids(): Promise<number[]> {
       }
     )
   })
-}
-
-/**
- * Send a signal to the given process id.
- *
- * @export
- * @param {number} pid the process id. Required.
- * @param {boolean} [force=false] if true, and the current user has
- * permissions to send the signal, the pid will be forced to shut down.
- */
-export function kill(pid: number | null | undefined, force = false): void {
-  if (pid == null) return
-
-  if (pid === process.pid || pid === process.ppid) {
-    throw new Error("cannot self-terminate")
-  }
-
-  if (isWin) {
-    const args = ["/PID", safePid(pid), "/T"]
-    if (force) {
-      args.push("/F")
-    }
-    child_process.execFile("taskkill", args)
-  } else {
-    try {
-      process.kill(pid, force ? "SIGKILL" : "SIGTERM")
-    } catch (err) {
-      if (!String(err).includes("ESRCH")) throw err
-    }
-  }
 }
