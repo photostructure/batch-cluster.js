a674f59a7042ae06a6159772ec02875775998a29
Matthew McEachen <matthew@photostructure.com>
2024-02-09 21:07:01 -0800

    handle zero timeouts as don't-timeout

--- src/Timeout.ts
+++ src/Timeout.ts
@@ -5,26 +5,30 @@ export async function thenOrTimeout<T>(
   p: Promise<T>,
   timeoutMs: number,
 ): Promise<T | typeof Timeout> {
-  return new Promise<T | typeof Timeout>(async (resolve, reject) => {
-    let pending = true
-    try {
-      const t = timers.setTimeout(() => {
-        if (pending) {
-          pending = false
-          resolve(Timeout)
+  // TODO: if timeoutMs is [1, 1000], it's probably a mistake. Should we do
+  // something else in that case?
+  return timeoutMs <= 1
+    ? p
+    : new Promise<T | typeof Timeout>(async (resolve, reject) => {
+        let pending = true
+        try {
+          const t = timers.setTimeout(() => {
+            if (pending) {
+              pending = false
+              resolve(Timeout)
+            }
+          }, timeoutMs)
+          const result = await p
+          if (pending) {
+            pending = false
+            clearTimeout(t)
+            resolve(result)
+          }
+        } catch (err) {
+          if (pending) {
+            pending = false
+            reject(err)
+          }
         }
-      }, timeoutMs)
-      const result = await p
-      if (pending) {
-        pending = false
-        clearTimeout(t)
-        resolve(result)
-      }
-    } catch (err) {
-      if (pending) {
-        pending = false
-        reject(err)
-      }
-    }
-  })
+      })
 }
