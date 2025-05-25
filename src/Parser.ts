e953b9a3c1b2d3c4642a126b36140079b27b048c
Matthew McEachen <matthew@photostructure.com>
2025-05-24 18:10:45 -0700

    refactor(Parser): update interface to type definition for clarity

--- src/Parser.ts
+++ src/Parser.ts
@@ -5,24 +5,26 @@ import { notBlank } from "./String"
  * process to a more useable format. This can be a no-op passthrough if no
  * parsing is necessary.
  */
-export interface Parser<T> {
-  /**
-   * Invoked once per task.
-   *
-   * @param stdout the concatenated stream from `stdin`, stripped of the `PASS`
-   * or `FAIL` tokens from `BatchProcessOptions`.
-   *
-   * @param stderr if defined, includes all text emitted to stderr.
-   *
-   * @param passed `true` iff the `PASS` pattern was found in stdout.
-   *
-   * @throws an error if the Parser implementation wants to reject the task. It
-   * is valid to raise Errors if stderr is undefined.
-   *
-   * @see BatchProcessOptions
-   */
-  (stdout: string, stderr: string | undefined, passed: boolean): T | Promise<T>
-}
+/**
+ * Invoked once per task.
+ *
+ * @param stdout the concatenated stream from `stdin`, stripped of the `PASS`
+ * or `FAIL` tokens from `BatchProcessOptions`.
+ *
+ * @param stderr if defined, includes all text emitted to stderr.
+ *
+ * @param passed `true` iff the `PASS` pattern was found in stdout.
+ *
+ * @throws an error if the Parser implementation wants to reject the task. It
+ * is valid to raise Errors if stderr is undefined.
+ *
+ * @see BatchProcessOptions
+ */
+export type Parser<T> = (
+  stdout: string,
+  stderr: string | undefined,
+  passed: boolean,
+) => T | Promise<T>
 
 export const SimpleParser: Parser<string> = (
   stdout: string,
