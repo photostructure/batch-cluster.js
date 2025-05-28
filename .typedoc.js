6d8f8b608471329b765cb99b2e9cb0be6b27e26e
Matthew McEachen <matthew@photostructure.com>
2025-05-28 10:19:53 -0700

    feat(docs): switch to GHA

--- .typedoc.js
+++ .typedoc.js
@@ -2,16 +2,10 @@ module.exports = {
   name: "batch-cluster",
   out: "./docs/",
   readme: "./README.md",
-  includes: "./src",
   gitRevision: "main", // < prevents docs from changing after every commit
   exclude: ["**/*test*", "**/*spec*"],
   excludePrivate: true,
   entryPoints: [
     "./src/BatchCluster.ts",
-    // "./src/BatchClusterOptions.ts",
-    // "./src/BatchProcessOptions.ts",
-    // "./src/Logger.ts",
-    // "./src/Task.ts",
-  ],
-
+  ]
 }
